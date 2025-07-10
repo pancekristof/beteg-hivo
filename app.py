9190c4d8d26bdc3372d15d082e12524ce6890bc0
pancekristof <pancekristof.pk@gmail.com>
2025-07-10 15:30:07 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,75 +1,33 @@
-from flask import Flask, render_template, request, jsonify
-from flask_socketio import SocketIO, emit
-import json
-import os
+# Tárolók
+data = {
+    "Bőrgyógyászat": [],
+    "Szemészet": [],
+    "Urológia": []
+}
+called_numbers = []
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
-
-DATA_FILE = 'data.json'
-
-# Betöltjük az adatokat fájlból vagy inicializálunk
-if os.path.exists(DATA_FILE):
-    with open(DATA_FILE, 'r') as f:
-        data = json.load(f)
-else:
-    data = {
-        "Bőrgyógyászat": [],
-        "Szemészet": [],
-        "Urológia": []
-    }
-
-def save_data():
-    with open(DATA_FILE, 'w') as f:
-        json.dump(data, f)
-
-@app.route('/')
-def index():
-    return "Beteghívó rendszer"
-
-@app.route('/input')
+@app.route('/input', methods=['GET', 'POST'])
 def input_page():
+    if request.method == 'POST':
+        option = request.form['option']
+        number = request.form['number']
+        if option in data:
+            data[option].append(number)
+        return redirect('/input')
     return render_template('input.html', options=list(data.keys()))
-
-@app.route('/display')
-def display_page():
-    return render_template('display.html')
 
 @app.route('/admin')
 def admin_page():
-    return render_template('admin.html', options=list(data.keys()))
+    return render_template('admin.html', data=data)
 
-@app.route('/add_number', methods=['POST'])
-def add_number():
-    content = request.json
-    option = content.get('option')
-    number = content.get('number')
-    if option in data:
-        data[option].append(number)
-        save_data()
-        socketio.emit('update', data)
-        return jsonify({"status": "ok"})
-    else:
-        return jsonify({"status": "error", "message": "Invalid option"}), 400
+@app.route('/display')
+def display_page():
+    return render_template('display.html', data=data, called=called_numbers)
 
-@app.route('/call_number', methods=['POST'])
+@app.route('/call')
 def call_number():
-    content = request.json
-    option = content.get('option')
-    number = content.get('number')
-    if option in data and number in data[option]:
-        data[option].remove(number)
-        save_data()
-        socketio.emit('update', data)
-        socketio.emit('call', {"option": option, "number": number})
-        return jsonify({"status": "ok"})
-    else:
-        return jsonify({"status": "error", "message": "Invalid option or number"}), 400
-
-@socketio.on('connect')
-def handle_connect():
-    emit('update', data)
-
-if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000)
+    option = request.args.get('option')
+    number = request.args.get('number')
+    if number and number not in called_numbers:
+        called_numbers.append(number)
+    return '', 204
