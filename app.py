700e3f10caad6740e4e98ce703fb0bc6717d0629
pancekristof <pancekristof.pk@gmail.com>
2025-07-10 15:35:05 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-# Tárolók
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+# Előre definiált állomások
 data = {
     "Bőrgyógyászat": [],
     "Szemészet": [],
     "Urológia": []
 }
+
+# Behívott sorszámok
 called_numbers = []
 
+# Input oldal - beteg beléptetés
 @app.route('/input', methods=['GET', 'POST'])
 def input_page():
     if request.method == 'POST':
@@ -16,14 +23,17 @@ def input_page():
         return redirect('/input')
     return render_template('input.html', options=list(data.keys()))
 
+# Admin oldal - hívásra alkalmas sorszámok
 @app.route('/admin')
 def admin_page():
     return render_template('admin.html', data=data)
 
+# Kijelző oldal - sorszámok megjelenítése
 @app.route('/display')
 def display_page():
     return render_template('display.html', data=data, called=called_numbers)
 
+# Behívás - admin oldalról történik
 @app.route('/call')
 def call_number():
     option = request.args.get('option')
@@ -31,3 +41,7 @@ def call_number():
     if number and number not in called_numbers:
         called_numbers.append(number)
     return '', 204
+
+# Fejlesztési mód helyi futtatásra
+if __name__ == '__main__':
+    app.run(debug=True)
